9ae0509cbd8d437911b447d9bd9f2852c99214d1
Michael Eden <edentastic@gmail.com>
2023-02-23 13:58:44 -0500

    calling backend in the front end

---
+++ lotr-vue/src/components/MovieContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="movie-content">
+    <h2>{{ movie.name }}</h2>
+    <ul>
+      <li>Runtime: {{ movie.runtimeInMinutes }} minutes</li>
+      <li>Budget: ${{ movie.budgetInMillions }} million</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'movie-content',
+  props: ["movie"],
+
+}
+</script>
+
+<style scoped>
+  .movie-content{
+    border: black 2px solid;
+    border-radius: 10px;
+    width: 250px;
+    height: 550px;
+    margin: 20px;
+
+    background-color: aquamarine;
+  }
+</style>
--- lotr-vue/src/views/MoviesView.vue
+++ lotr-vue/src/views/MoviesView.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
      <h1>Movies</h1>
+     <div class="movie-container" >
+      <movie-content 
+      v-for="movie in movies" 
+      :key="movie._id" 
+      :movie="movie" 
+      />
+     </div>
   </div>
 </template>
 
 <script>
+import MovieContent from '@/components/MovieContent.vue'
+import backendService from '../services/BackendService.js'
 export default {
-
+  components: { MovieContent },
+  name: 'movies-view',
+  data(){
+    return{
+      movies: [],
+    }
+  },
+  created(){
+    backendService.viewMoviesFromAPI().then(response=>{
+      if(response.status==200){
+        this.movies=response.data;
+      }
+    })
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .movie-container{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
 </style>
